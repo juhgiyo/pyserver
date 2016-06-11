91c70076be3b46c3915c1205878376b4db6d4b9c
Woong Gyu La <juhgiyo@gmail.com>
2016-06-11 15:40:58 +0900

    Update pip distribution

--- setup.py
+++ setup.py
@@ -1,19 +1,26 @@
-from distutils.core import setup
+from setuptools import setup,find_packages
 try:
     with open('VERSION.txt', 'r') as v:
         version = v.read().strip()
-except FileNotFoundError:
+except Exception:
     version = '0.0.0-dev'
 
 setup(
   name = 'pyserver',
-  packages = ['pyserver'], # this must be the same as the name above
+  packages = find_packages(exclude=["dist"]),
   description = 'TCP/UDP Aysnchronous Server/Client Library',
   author = 'Woong Gyu La',
   author_email = 'juhgiyo@gmail.com',
   version=version,
   url = 'https://github.com/juhgiyo/pyserver', # use the URL to the github repo
-    keywords = ['tcp', 'udp', 'server', 'library'], # arbitrary keywords
-  license='MIT',
-  classifiers = [],
+  keywords = ['tcp', 'udp', 'server', 'library'], # arbitrary keywords
+  license="The MIT License (MIT)",
+  classifiers = [
+    # How mature is this project? Common values are
+    #   3 - Alpha
+    #   4 - Beta
+    #   5 - Production/Stable
+    'Development Status :: 4 - Beta',
+    'Programming Language :: Python :: 2.7',
+  ],
 )
